21b74f75401435279f7ef478f41ad18c0fe6c242
Tosha177777 <slavkoanton4@gmail.com>
2023-11-15 18:13:29 +0200

    first try

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,15 @@
 /*
-  Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
-*/
-
-class Component {
-  constructor (public props:T) {
-
-  }
+    Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
+  */
+interface ComponentProps {
+  [key: string]: string;
+}
+class Component<T extends ComponentProps> {
+  constructor(public props: T) {}
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<ComponentProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
